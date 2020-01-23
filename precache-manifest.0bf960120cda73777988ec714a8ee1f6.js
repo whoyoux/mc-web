c32419f73560ab2f7f71501bab9e460c3821f31f
whoyoux <kamil@tecza.hub.pl>
2020-01-23 20:06:09 +0100

    Updates

---
+++ precache-manifest.0bf960120cda73777988ec714a8ee1f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb4d60506f0b3cb5ad7e76fbf95dd0",
+    "url": "/mc-web/index.html"
+  },
+  {
+    "revision": "9c0680daea71d6aa0021",
+    "url": "/mc-web/static/css/main.35a71dce.chunk.css"
+  },
+  {
+    "revision": "1e30529470a14bfa02fb",
+    "url": "/mc-web/static/js/2.7bfc1c1d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mc-web/static/js/2.7bfc1c1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c0680daea71d6aa0021",
+    "url": "/mc-web/static/js/main.8bf9b9d5.chunk.js"
+  },
+  {
+    "revision": "39cc79ebd9fdcc7878bc",
+    "url": "/mc-web/static/js/runtime-main.c28adf30.js"
+  },
+  {
+    "revision": "d33e145e9385530619ef7f4213684daa",
+    "url": "/mc-web/static/media/img1.d33e145e.jpg"
+  },
+  {
+    "revision": "ad228c949a31e4df96c9f125ee6e392c",
+    "url": "/mc-web/static/media/img2.ad228c94.jpg"
+  },
+  {
+    "revision": "e1db1483d6ead0ef9dbb42b6edc7bcae",
+    "url": "/mc-web/static/media/img3.e1db1483.jpg"
+  },
+  {
+    "revision": "90748621d10df779ff81d43831cad68b",
+    "url": "/mc-web/static/media/team.90748621.jpg"
+  }
+]);
